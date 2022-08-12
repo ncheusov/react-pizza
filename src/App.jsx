afc72534bfe6c6dd08cb05f4ca5853705d74a4d6
Nikita <ncheusov@inbox.ru>
2022-08-12 23:43:55 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+
+import './scss/app.scss';
+
+function App() {
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <PizzaBlock />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
